cca0dd27c520d39dfb3b14518f8144dcce20be50
Panachai Vankwan <s6101012620105@email.kmutnb.ac.th>
2020-10-07 22:39:14 +0700

    Suggestion Project 4.2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,59 +1,53 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.all.order('title')
+    @cancel_path = movies_path
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.html.haml by default
+    id = params[:id]
+    begin
+      @movie = Movie.find(id)
+      @cancel_path = movie_path(@movie)
+    rescue ActiveRecord::RecordNotFound
+      redirect_to movies_path
+    end
   end
 
   def new
-    @movie = Movie.new
-    # default : render 'new ' template
   end
 
   def create
-    params.require(:movie)
-    params[:movie].permit(:title,:rating,:release_date)
-
-    movie_1 = {:title => 'test', :rating => 'G',
-      :release_date => '25-Nov-2000'}
-    # @movie = Movie.create!(movie_1)      
-    
-    test01 = params[:movie]
-
-    movie01 = {:title => test01["title"], :rating => test01["rating"],
-      :release_date => "#{test01["release_date(1i)"]}-#{test01["release_date(2i)"]}-#{test01["release_date(3i)"]}"}
-
-    # flash[:notice] = "#{movie_1} #{params[:movie]} #{@movie}"
-
-    @movie = Movie.create!(movie01)
-    flash[:notice] = "#{test01["title"]} was successfully created."
-    redirect_to movies_path
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} #{@movie.id} was successfully created."
+    redirect_to movie_path(@movie)
   end
 
   def edit
-    @movie = Movie.find params[:id]
+    id = params[:id]
+    @movie = Movie.find(id)
+    @cancel_path = movie_path(@movie)
   end
 
   def update
-    @movie = Movie.find params[:id]
-    getParamMovie = params[:movie]
-    movie01 = {:title => getParamMovie["title"], :rating => getParamMovie["rating"],
-      :release_date => "#{getParamMovie["release_date(1i)"]}-#{getParamMovie["release_date(2i)"]}-#{getParamMovie["release_date(3i)"]}"}
-    
-    @movie.update_attributes!(movie01)    
-    # flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    respond_to do |client_wants|
+      client_wants.html { redirect_to movies_path }
+      client_wants.xml { render :xml => @movie.xml }
+    end
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash[:notice] = "#{@movie.title} was successfully deleted."
     redirect_to movies_path
   end
-      
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :release_date, :description)
+  end
 end
