a875cc1193d6108be3290007ffd4254108a2ed13
parkin <tatazakup_tatazakup@hotmail.com>
2020-10-13 03:42:09 -0700

    fix config Oauth-4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,24 @@
 class SessionsController < ApplicationController
-# user shouldn't have to be logged in before logging in!
-skip_before_action :set_current_user
-skip_before_action :authenticate!
+  # user shouldn't have to be logged in before logging in!
+  skip_before_action :set_current_user
+  skip_before_action :authenticate!
 
-def create
-auth=request.env["omniauth.auth"]
-user=Moviegoer.find_by(:provider => auth["provider"], :uid => auth["uid"]) ||
+  def create
+    auth=request.env["omniauth.auth"]
+    user=Moviegoer.find_by(:provider => auth["provider"], :uid => auth["uid"]) || Moviegoer.create_with_omniauth(auth)
 
-unless user
-  user = Moviegoer.create_with_omniauth!(auth)
-end
+    unless user
+      user = Moviegoer.create_with_omniauth!(auth)
+    end
 
-session[:user_id] = user.id
-redirect_to movies_path
-end
+    session[:user_id] = user.id
+    redirect_to movies_path
+  end
 
-def destroy
-session.delete(:user_id)
-flash[:notice] = 'Logged out successfully.'
-redirect_to movies_path
-end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'Logged out successfully.'
+    redirect_to movies_path
+  end
 
 end
